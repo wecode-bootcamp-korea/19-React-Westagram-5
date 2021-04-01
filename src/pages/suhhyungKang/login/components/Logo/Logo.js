b80d56c2fcaec2150db26c9b257db69df54c90ea
강서형 <syeongii@gangseohyeong-ui-MacBookPro.local>
2021-04-01 15:43:44 +0900

    ADD: Login 파일에 폴더추가 및 components 나누기

---
+++ src/pages/suhhyungKang/login/components/Logo/Logo.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+//자식
+class Logo extends Component {
+  render(){
+    return(
+      <div className="colorBox">
+      <h1>Westagram</h1>
+      <div className="colorBox2">
+          <input className="inputId" type="text" placeholder="전화번호,사용자 이름 또는 이메일"/>
+          <input className="inputPw" type="password" placeholder="비밀번호"/>
+          <button className="login">로그인</button>
+          <div className="underBox">
+              <div className="line"></div>
+              <div className="or">또는</div>
+              <div className="line"></div>
+          </div>
+      </div>
+      <button className="faceBook">
+          <i className="fab fa-facebook-square"></i>
+          <a className="face" href="https://www.facebook.com/login">Facebook으로 로그인</a>
+      </button>
+      <a className="forGet" href="https://www.instagram.com/accounts/password/reset/">비밀번호를 잊으셨나요?</a>
+  </div>
+    );
+  }
+}
+
+export default Logo;
