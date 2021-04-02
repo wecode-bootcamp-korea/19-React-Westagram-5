a0df954e8a19c48ae1b582be3d1203e222319255
Dongee Seo <dongeeseo@Dongeeui-MacBook-Pro.local>
2021-04-02 19:08:09 +0900

    초기세팅 수정

--- src/Routes.js
+++ src/Routes.js
@@ -5,24 +5,23 @@ import {
   Route,
 } from 'react-router-dom';
 
-import donghyunkimLogin from './pages/donghyunkimlogin/Login';
-import donghyunkimMain from './pages/donghyunkimmain/Main';
-import dongeeseoLogin from './pages/dongeeseologin/Login';
-import dongeeseoMain from './pages/dongeeseomain/Main';
-import seriparkLogin from '.pages/seriparklogin/Login';
-import seriparkMain from './pages/seriparkmain/Main';
-import suhhyungKangLogin from './pages/suhhyungKanlogin/Login';
-import suhhyungKangMain from './pages/suhhyungKangmain/Main';
+import donghyunkimLogin from './pages/donghyunkim/login/Login';
+import donghyunkimMain from './pages/donghyunkim/main/Main';
+import dongeeseoLogin from './pages/dongeeseo/login/Login';
+import dongeeseoMain from './pages/dongeeseo/main/Main';
+import seriparkLogin from './pages/seripark/login/Login';
+import seriparkMain from './pages/seripark/main/Main';
+import suhhyungKangLogin from './pages/suhhyungKang/login/Login';
+import suhhyungKangMain from './pages/suhhyungKang/main/Main';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path= "/" component={teamindex} />
           <Route exact path="/logindh" component={donghyunkimLogin} />
-          <Route exact path="/maindh" component={donghyunkimMain} />
-          <Route exact path = "/loginsd" component = {dongeeseoLogin} />
+          <Route exact path="/maindh" component={donghyunkimMain} /> 
+          <Route exact path="/loginsd" component={dongeeseoLogin} />
           <Route exact path="/mainsd" component={dongeeseoMain} />
           <Route exact path="/loginsp" component={seriparkLogin} />
           <Route exact path="/mainsp" component={seriparkMain} />
--- src/pages/dongeeseo/login/Login.js
+++ src/pages/dongeeseo/login/Login.js
@@ -1,46 +1,57 @@
+import React from 'react' ;
+import { withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './Login.scss';
 
-import React, {Component} from 'react' ;
 
 
-class Login extends Component {
-    render() {
+
+class Login extends React.Component {
+
+    goToMain = () => {
+        this.props.history.push('/mainsd');
+    }
+    
+    render() { 
         return (
-        <>
-            <header>
-                <h1> Westagram </h1>
-                <main className = "login">
-                    <input className = "Id" type = "text" placeholder = "Phone number, username, or email" />
-                    <br/>
-                    <input className = "Pw" type = "password"  placeholder = "Password" />
-                    <br/>
-                    <button className = "logInButton" type = "button"></button>
-                </main>
-                <div className = "orLine">
-                    <div className = "sideLine"></div>
-                    <div className = "orLetter"> OR </div>
-                    <div className = "sideLine"></div>
+       <>
+       <main>
+            <div className = "header">
+             <h1> Westagram </h1>
+            </div>
+            <form>
+                <input className = "Id" type = "text" placeholder = "Phone number, username, or email" />
+                <input className = "Pw" type = "password"  placeholder = "Password" />
+                <button className = "logInButton" type = "button" onClick={this.goToMain}>
+                Log In       
+                </button>
+            </form>
+            <div className = "orLine">
+                <div className = "sideLine"></div>
+                <div className = "orLetter"> OR </div>
+                <div className = "sideLine"></div>
+            </div>
+            <nav>
+                <div className = "facebook">
+                    <a href = "https://www.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26state%3D%257B%2522fbLoginKey%2522%253A%25221m8wh6j1ptef7i1tvk8zls8sc991gu36x715dghvk15gfprp1gzar4i%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252F%2522%257D%26scope%3Demail%26response_type%3Dcode%252Cgranted_scopes%26locale%3Den_US%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dcca89f78-d25b-4d83-8e32-822d37821a88%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%25221m8wh6j1ptef7i1tvk8zls8sc991gu36x715dghvk15gfprp1gzar4i%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252F%2522%257D%23_%3D_&display=page&locale=ko_KR&pl_dbl=0">
+                            <i className="fab fa-facebook-square"></i>
+                            <span> Log in with Facebook </span>
+                    </a>
                 </div>
-                <nav className = "otherMethod">
-                    <div className = "facebook">
-                        <a href = "https://www.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26state%3D%257B%2522fbLoginKey%2522%253A%25221m8wh6j1ptef7i1tvk8zls8sc991gu36x715dghvk15gfprp1gzar4i%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252F%2522%257D%26scope%3Demail%26response_type%3Dcode%252Cgranted_scopes%26locale%3Den_US%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dcca89f78-d25b-4d83-8e32-822d37821a88%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%25221m8wh6j1ptef7i1tvk8zls8sc991gu36x715dghvk15gfprp1gzar4i%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252F%2522%257D%23_%3D_&display=page&locale=ko_KR&pl_dbl=0">
-                                <i className="fab fa-facebook-square"></i>
-                                <span> Log in with Facebook </span>
-                        </a>
-                    </div>
-                    <br/>
-                    <div className = "forgotPw">
-                        <a href = ""> Forgot password?</a>
-                    </div>
-                </nav>
-            </header>
-            <section className = "signUp">
+                <div className = "forgotPw">
+                    <Link to="/Main">Forgot password?</Link>
+                </div>
+            </nav>
+        </main>    
+        <section className = "signUp">
                 <span> Don't have an account?
-                    <a href = ""> Sign up </a>
+                    <a href = "https://apps.apple.com/app/instagram/id389801252?vt=lo"> Sign up </a>
                 </span>
-            </section>
+        </section>
+        <form>
             <div class = "getAppBox">
                 <div className = "getAppLetter">
-                    <a> Get the app. </a>
+                    <a href = "https://apps.apple.com/app/instagram/id389801252?vt=lo"> Get the app. </a>
                 </div>
                 <div className = "getAppStore">
                     <a className = "appleStore" href = "https://apps.apple.com/app/instagram/id389801252?vt=lo">
@@ -51,9 +62,10 @@ class Login extends Component {
                     </a>
                 </div>
             </div>
+        </form>
         </>
         )
     }
 }
 
-export default Login
+export default withRouter(Login)
