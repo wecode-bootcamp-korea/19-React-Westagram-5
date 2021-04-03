5c73821502827fbee8e908e4661dc3ac8c08c650
moripark32 <moripark32@gmail.com>
2021-04-03 21:30:41 +0900

    finished login.js & .scss

--- src/pages/seripark/login/Login.js
+++ src/pages/seripark/login/Login.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react'
 import './Login.scss';
 
+
 class Login extends Component {
     render() {
       return (
-        <div>
-          <header>
-            <h1 class="logo_title">Instagram</h1>
-          </header>
-          <form id="form" method="get">
-            <input type="text" id="id_text" name="login_id_text" placeholder="전화번호,사용자 이름 또는 이메일"/>
-            <input type="text" id="pw_text" name="login_pw_text" placeholder="비밀번호"/>
-            <input type="submit" id="login_btn" value="로그인"/>
-          </form>
-          <footer>
-            <a href="./" class="find_pw">비밀번호를 잊으셨나요?</a>
-          </footer>   
+        <div class="login_background">
+          <div class="container">
+            <div class="login_logo_wrap">
+              <h1 class="login_logo_title">Instagram</h1>
+            </div>
+            <form id="login_form" method="get">
+              <input type="text" id="id_text" name="login_id_text" placeholder="전화번호,사용자 이름 또는 이메일"/>
+              <input type="text" id="pw_text" name="login_pw_text" placeholder="비밀번호"/>
+              <input type="submit" id="login_btn" value="로그인"/>
+            </form>
+            <footer>
+              <a href="./" class="find_pw">비밀번호를 잊으셨나요?</a>
+            </footer>
+          </div>
         </div>      
       )
     }
 }
 
 export default Login
+
+
+
