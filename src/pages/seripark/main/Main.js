87c2cdf92b8ffb331217f28ff48da328faf06497
moripark32 <moripark32@gmail.com>
2021-04-05 23:14:13 +0900

    Main.js in the middle of work

--- src/pages/seripark/main/Main.js
+++ src/pages/seripark/main/Main.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React,{Component} from 'react';
 import MainHeader from './Components/MainHeader/MainHeader';
 import MainCon1 from './Components/MainCon1/MainCon1';
 import MainCon2 from './Components/MainCon2/MainCon2';
-import Dog from "images/seripark/download.jpg";
-  class Main extends Component{
+
+  class Login extends Component{
     render(){
       return(
         <>
@@ -16,4 +16,4 @@ import Dog from "images/seripark/download.jpg";
       )
     }
   }
-export default Main;
+export default Login;
